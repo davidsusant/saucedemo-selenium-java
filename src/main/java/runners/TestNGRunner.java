afc5c6ee1d342c815de30feae381a955655669d2
David Susanto <david.susanto@stockbit.com>
2025-04-17 18:47:25 +0700

    testng runner

---
+++ src/main/java/runners/TestNGRunner.java
@@ -0,0 +1,23 @@
+package runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "src/main/java/stepdefs",
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports/cucumber.html",
+                "json:target/cucumber-reports/cucumber.json",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+        }
+)
+public class TestNGRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}
--- src/main/java/stepdefs/Hooks.java
+++ src/main/java/stepdefs/Hooks.java
@@ -2,17 +2,25 @@ package stepdefs;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.BeforeStep;
 import io.cucumber.java.Scenario;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import pages.LoginPage;
 
 public class Hooks {
     private static WebDriver driver;
     private static LoginPage loginPage;
+
+    @BeforeSuite
+    public void beforeSuite() {
+
+    }
 
     @Before
     public void setUp() {
@@ -34,5 +42,10 @@ public class Hooks {
         if (driver != null) {
             driver.quit();
         }
+    }
+
+    @AfterSuite
+    public void afterSuite() {
+
     }
 }
